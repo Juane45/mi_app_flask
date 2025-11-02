3f3f2e7ef98f7730ed5c1275ffe4d6184d275319
Juane45 <jsalvatore@unaj.edu.ar>
2025-11-02 17:57:47 -0300

    Corrijo error __name__

--- mi_app_fask.py
+++ mi_app_fask.py
@@ -1,13 +1,13 @@
 from flask import Flask
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hola, esta es mi red local de ejemplo!"
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     # Usar el puerto que Render asigne, por defecto 5000 si no hay
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
